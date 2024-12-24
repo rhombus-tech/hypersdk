8ed61400c516532d4778217fbe5b82792832ff77
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-23 23:44:54 -0500

    Create option.go
    Signed-off-by: MuffinsThaCat <talzisckind19@gmail.com>

---
+++ api/region/option.go
@@ -0,0 +1,27 @@
+package region
+
+import (
+    "github.com/ava-labs/hypersdk/api"
+    "github.com/ava-labs/hypersdk/vm"
+)
+
+const Namespace = "region"
+
+type Config struct {
+    Enabled bool `json:"enabled"`
+}
+
+func NewDefaultConfig() Config {
+    return Config{
+        Enabled: true,
+    }
+}
+
+func With() vm.Option {
+    return vm.NewOption(Namespace, NewDefaultConfig(), func(v api.VM, config Config) (vm.Opt, error) {
+        if !config.Enabled {
+            return vm.NewOpt(), nil
+        }
+        return vm.WithVMAPIs(ServerFactory{}), nil
+    })
+}
