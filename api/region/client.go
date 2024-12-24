a26709809af10e8bf38a751769a468abeb3e1323
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-24 04:43:39 -0500

    Update client.go
    Signed-off-by: MuffinsThaCat <talzisckind19@gmail.com>

--- api/region/client.go
+++ api/region/client.go
@@ -1,60 +1,28 @@
+// region/config.go
 package region
 
 import (
-    "context"
-    "strings"
-
     "github.com/ava-labs/hypersdk/api"
-    "github.com/ava-labs/hypersdk/requester" 
+    "github.com/ava-labs/hypersdk/vm"
 )
 
-type Client struct {
-    requester *requester.EndpointRequester
+const Namespace = "region"
+
+type Config struct {
+    Enabled bool `json:"enabled"`
 }
 
-func NewClient(uri string) *Client {
-    uri = strings.TrimSuffix(uri, "/")
-    uri += Endpoint
-    req := requester.New(uri, Name)
-    return &Client{requester: req}
+func NewDefaultConfig() Config {
+    return Config{
+        Enabled: true,
+    }
 }
 
-func (c *Client) GetRegion(ctx context.Context, regionID string) (*GetRegionResponse, error) {
-    resp := new(GetRegionResponse)
-    err := c.requester.SendRequest(
-        ctx,
-        "getRegion",
-        &GetRegionRequest{RegionID: regionID},
-        resp,
-    )
-    return resp, err
-}
-
-func (c *Client) SubmitRegionalTx(ctx context.Context, regionID string, txBytes []byte) (*SubmitRegionalTxResponse, error) {
-    resp := new(SubmitRegionalTxResponse) 
-    err := c.requester.SendRequest(
-        ctx,
-        "submitRegionalTx",
-        &SubmitRegionalTxRequest{
-            RegionID: regionID,
-            TxBytes: txBytes,
-        },
-        resp,
-    )
-    return resp, err
-}
-
-func (c *Client) VerifyAttestation(ctx context.Context, regionID string, enclaveID []byte, attestation []byte) (*VerifyAttestationResponse, error) {
-    resp := new(VerifyAttestationResponse)
-    err := c.requester.SendRequest(
-        ctx,
-        "verifyAttestation", 
-        &VerifyAttestationRequest{
-            RegionID: regionID,
-            EnclaveID: enclaveID,
-            Attestation: attestation,
-        },
-        resp,
-    )
-    return resp, err
+func With() vm.Option {
+    return vm.NewOption(Namespace, NewDefaultConfig(), func(v api.VM, config Config) (vm.Opt, error) {
+        if !config.Enabled {
+            return vm.NewOpt(), nil
+        }
+        return vm.WithVMAPIs(ServerFactory{}), nil
+    })
 }
