669a621368e53446bc97cf8e3b5fede6dc4cf223
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-24 04:47:15 -0500

    Update ws.go
    Signed-off-by: MuffinsThaCat <talzisckind19@gmail.com>

--- api/region/ws.go
+++ api/region/ws.go
@@ -1,27 +1,66 @@
 package region
 
 import (
-    "context"
-    
-    "github.com/ava-labs/hypersdk/pubsub"
+  "context"
+  "encoding/json"
+  
+  "github.com/ava-labs/hypersdk/pubsub"
 )
 
 const (
-    RegionEventMode byte = 0x20
+  RegionEventMode byte = 0x20
+
+  // Event types
+  RegionTEEStatus    = "tee-status"
+  RegionExecution    = "execution"
+  RegionCrossRegion  = "cross-region"
 )
 
 type RegionEvent struct {
-    RegionID string
-    EventType string 
-    Data []byte
+  RegionID    string          `json:"regionId"`
+  EventType   string          `json:"eventType"`
+  Timestamp   string          `json:"timestamp"`
+  Data        json.RawMessage `json:"data"`
 }
 
-func (s *Server) RegisterRegionEvents(conn *pubsub.Connection, regionID string) error {
-    // Implementation to register for region events
-    return nil
+// Server-side registration
+func (s *Server) RegionEventManager(ctx context.Context) error {
+  channel := s.vm.EventManager().RegisterChannel(RegionEventMode)
+  for {
+      select {
+      case <-ctx.Done():
+          return ctx.Err()
+      case e := <-channel:
+          event := new(RegionEvent)
+          if err := json.Unmarshal(e, event); err != nil {
+              continue
+          }
+          // Process event
+      }
+  }
 }
 
-func (c *Client) ListenRegionEvents(ctx context.Context) (*RegionEvent, error) {
-    // Implementation to listen for region events
-    return nil, nil
+// Client-side registration
+func (c *Client) ListenRegionEvents(ctx context.Context, regionID string) (<-chan *RegionEvent, error) {
+  eventChan := make(chan *RegionEvent)
+  
+  err := c.ws.RegisterChannel(RegionEventMode, func(msg []byte) error {
+      event := new(RegionEvent)
+      if err := json.Unmarshal(msg, event); err != nil {
+          return err
+      }
+      if event.RegionID == regionID {
+          select {
+          case eventChan <- event:
+          default:
+          }
+      }
+      return nil
+  })
+  
+  if err != nil {
+      return nil, err
+  }
+
+  return eventChan, nil
 }
